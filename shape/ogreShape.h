ffa9f4399b3739af3ea58f6bafe15effcaf8e0b3
jbreslin33 <jbreslin33@localhost>
2011-05-05 15:00:01 -0400

    cleaningk

--- shape/ogreShape.h
+++ shape/ogreShape.h
@@ -1,13 +1,6 @@
 #ifndef OGRESHAPE_H
 #define OGRESHAPE_H
 
-#define NUM_ANIMS 13           // number of animations the character has
-#define CHAR_HEIGHT 5          // height of character's center of mass above ground
-#define CAM_HEIGHT 2           // height of camera above character's center of mass
-#define RUN_SPEED 17           // character running speed in units per second
-#define TURN_SPEED 500.0f      // character turning in degrees per second
-#define ANIM_FADE_SPEED 7.5f   // animation crossfade speed in % of full weight per second
-
 #include "shape.h"
 
 #include "Ogre.h"
