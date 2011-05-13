e1906ba10b65d333a887e0c1ffb145df6a67c34f
jbreslin33 <jbreslin33@localhost>
2011-05-12 21:02:35 -0400

    milestone.

--- player/states/moveStates.cpp
+++ player/states/moveStates.cpp
@@ -20,56 +20,52 @@ void Normal_Move::enter(ClientSidePlayer* player)
 }
 void Normal_Move::execute(ClientSidePlayer* player)
 {
-
 	float deltaX = player->mClient->mServerFrame.mOrigin.x - player->mShape->getSceneNode()->getPosition().x;
     float deltaZ = player->mClient->mServerFrame.mOrigin.z - player->mShape->getSceneNode()->getPosition().z;
 
 	//distance we are off from server
 	float dist = sqrt(pow(deltaX, 2) + pow(deltaZ, 2));
 
-	  // if distance exceeds threshold
-	  if(dist > player->posInterpLimitHigh)
-	   {
-          player->mClient->mCommand.mCatchup = true;
-	   }
+	// if distance exceeds threshold
+	if(dist > player->posInterpLimitHigh)
+	{
+		player->mClient->mCommand.mCatchup = true;
+	}
 
-	   //if we are back in sync
-	   if(dist < player->posInterpLimitLow)
-          player->mClient->mCommand.mCatchup = false;
-
-	   // if server has come to a stop
-	   if(player->mClient->mServerFrame.mVelocity.x == 0.0 && player->mClient->mServerFrame.mVelocity.z == 0.0)
-	   {
-		   player->mClient->mCommand.mStop = true;
-	   }
-	   else //server still moving
-	   {
-          player->mClient->mCommand.mStop = false;
-	   }
+	//if we are back in sync
+	if(dist < player->posInterpLimitLow)
+	{
+		player->mClient->mCommand.mCatchup = false;
+	}
+	   
+	// if server has come to a stop
+	if(player->mClient->mServerFrame.mVelocity.x == 0.0 && player->mClient->mServerFrame.mVelocity.z == 0.0)
+	{
+		player->mClient->mCommand.mStop = true;
+	}
+	else //server still moving
+	{
+		player->mClient->mCommand.mStop = false;
+	}
     
-	   //if server moving and client needs to catchup
-	   if(player->mClient->mCommand.mCatchup == true && player->mClient->mCommand.mStop == false)
-	   {
-		   //this is catcup state...
-		   player->moveStateMachine->changeState(Catchup_Move::Instance());
-	   }
-	   else //server stopped or we are in sync so just use server vel as is
-	   {
-		  //this is normal state...
+	//if server moving and client needs to catchup change to catchup state
+	if(player->mClient->mCommand.mCatchup == true && player->mClient->mCommand.mStop == false)
+	{
+		player->moveStateMachine->changeState(Catchup_Move::Instance());
+	}
+	else //server stopped or we are in sync so just use server vel as is, this is meat of normal state...
+	{
+		Ogre::Vector3 serverDest  = Ogre::Vector3::ZERO;
+		Ogre::Vector3 myDest      = Ogre::Vector3::ZERO;
 
+	    serverDest.x = player->mClient->mServerFrame.mVelocity.x;
+		serverDest.z = player->mClient->mServerFrame.mVelocity.z;
+		serverDest.normalise();
+        serverDest = serverDest * player->runSpeed/1000.0;
 
-          Ogre::Vector3 serverDest  = Ogre::Vector3::ZERO;
-		  Ogre::Vector3 myDest      = Ogre::Vector3::ZERO;
-
-	      serverDest.x = player->mClient->mServerFrame.mVelocity.x;
-		  serverDest.z = player->mClient->mServerFrame.mVelocity.z;
-		  serverDest.normalise();
-          serverDest = serverDest * player->runSpeed/1000.0;
-
-		  player->mClient->mCommand.mVelocity.x = serverDest.x;
-	      player->mClient->mCommand.mVelocity.z = serverDest.z;
-
-	   }
+		player->mClient->mCommand.mVelocity.x = serverDest.x;
+	    player->mClient->mCommand.mVelocity.z = serverDest.z;
+	}
 }
 void Normal_Move::exit(ClientSidePlayer* player)
 {
@@ -86,74 +82,74 @@ void Catchup_Move::enter(ClientSidePlayer* player)
 }
 void Catchup_Move::execute(ClientSidePlayer* player)
 {
-
 	float deltaX = player->mClient->mServerFrame.mOrigin.x - player->mShape->getSceneNode()->getPosition().x;
     float deltaZ = player->mClient->mServerFrame.mOrigin.z - player->mShape->getSceneNode()->getPosition().z;
 
 	//distance we are off from server
 	float dist = sqrt(pow(deltaX, 2) + pow(deltaZ, 2));
 
-	  // if distance exceeds threshold
-	  if(dist > player->posInterpLimitHigh)
-	   {
-          player->mClient->mCommand.mCatchup = true;
-	   }
+	// if distance exceeds threshold
+	if(dist > player->posInterpLimitHigh)
+	{
+		player->mClient->mCommand.mCatchup = true;
+	}
 
-	   //if we are back in sync
-	   if(dist < player->posInterpLimitLow)
-          player->mClient->mCommand.mCatchup = false;
-
-	   // if server has come to a stop
-	   if(player->mClient->mServerFrame.mVelocity.x == 0.0 && player->mClient->mServerFrame.mVelocity.z == 0.0)
-	   {
-		   player->mClient->mCommand.mStop = true;
-	   }
-	   else //server still moving
-	   {
-          player->mClient->mCommand.mStop = false;
-	   }
+	//if we are back in sync
+	if(dist < player->posInterpLimitLow)
+	{
+		player->mClient->mCommand.mCatchup = false;
+	}
+	   
+	// if server has come to a stop
+	if(player->mClient->mServerFrame.mVelocity.x == 0.0 && player->mClient->mServerFrame.mVelocity.z == 0.0)
+	{
+		player->mClient->mCommand.mStop = true;
+	}
+	else //server still moving
+	{
+		player->mClient->mCommand.mStop = false;
+	}
     
-	   //if server moving and client needs to catchup
-	   if(player->mClient->mCommand.mCatchup == true && player->mClient->mCommand.mStop == false)
-	   {
-		    Ogre::Vector3 serverDest  = Ogre::Vector3::ZERO; //vector to future server pos
-		    Ogre::Vector3 myDest      = Ogre::Vector3::ZERO; //vector from clienr pos to future server pos
+	//if server moving and client needs to catchup
+	if(player->mClient->mCommand.mCatchup == true && player->mClient->mCommand.mStop == false)
+	{
+		Ogre::Vector3 serverDest  = Ogre::Vector3::ZERO; //vector to future server pos
+		Ogre::Vector3 myDest      = Ogre::Vector3::ZERO; //vector from clienr pos to future server pos
 
-			serverDest.x = player->mClient->mServerFrame.mVelocity.x;
-			serverDest.z = player->mClient->mServerFrame.mVelocity.z;
-			serverDest.normalise();
+		serverDest.x = player->mClient->mServerFrame.mVelocity.x;
+		serverDest.z = player->mClient->mServerFrame.mVelocity.z;
+		serverDest.normalise();
 
-			float multiplier = dist * player->posInterpFactor;
-			serverDest = serverDest * multiplier;
-			serverDest.x = player->mClient->mServerFrame.mOrigin.x + serverDest.x;
-			serverDest.z = player->mClient->mServerFrame.mOrigin.z + serverDest.z;
+		float multiplier = dist * player->posInterpFactor;
+		serverDest = serverDest * multiplier;
+		serverDest.x = player->mClient->mServerFrame.mOrigin.x + serverDest.x;
+		serverDest.z = player->mClient->mServerFrame.mOrigin.z + serverDest.z;
 
-			myDest.x = serverDest.x - player->mShape->getSceneNode()->getPosition().x;
-			myDest.z = serverDest.z - player->mShape->getSceneNode()->getPosition().z;
+		myDest.x = serverDest.x - player->mShape->getSceneNode()->getPosition().x;
+		myDest.z = serverDest.z - player->mShape->getSceneNode()->getPosition().z;
 
-            //dist from clienr pos to future server pos
-			float predictDist = pow(myDest.x, 2) + pow(myDest.z, 2);
-			predictDist = sqrt(predictDist);
+        //dist from clienr pos to future server pos
+		float predictDist = pow(myDest.x, 2) + pow(myDest.z, 2);
+		predictDist = sqrt(predictDist);
 
-			//server velocity
-			float vel = sqrt(pow(player->mClient->mServerFrame.mVelocity.x, 2) + pow(player->mClient->mServerFrame.mVelocity.z, 2))/player->mClient->mCommand.mMilliseconds;
-			//time needed to get to future server pos
-			float time = dist * player->posInterpFactor/(player->runSpeed/1000.0);
+		//server velocity
+		float vel = sqrt(pow(player->mClient->mServerFrame.mVelocity.x, 2) + pow(player->mClient->mServerFrame.mVelocity.z, 2))/player->mClient->mCommand.mMilliseconds;
+		//time needed to get to future server pos
+		float time = dist * player->posInterpFactor/(player->runSpeed/1000.0);
 
-			myDest.normalise();
+		myDest.normalise();
 
-			//client vel needed to get to future server pos in time
-			myDest = myDest * predictDist/time;
+		//client vel needed to get to future server pos in time
+		myDest = myDest * predictDist/time;
 
-			player->mClient->mCommand.mVelocity.x = myDest.x;
-	        player->mClient->mCommand.mVelocity.z = myDest.z;
-	   }
-	   else //server stopped or we are in sync so just use server vel as is
-	   {
-
-		   //this is normal state...
-		   player->moveStateMachine->changeState(Normal_Move::Instance());
-	   }
+		player->mClient->mCommand.mVelocity.x = myDest.x;
+	    player->mClient->mCommand.mVelocity.z = myDest.z;
+	}
+	else //server stopped or we are in sync so just use server vel as is
+	{
+		//this is normal state...
+		player->moveStateMachine->changeState(Normal_Move::Instance());
+	}
 }
 void Catchup_Move::exit(ClientSidePlayer* player)
 {
