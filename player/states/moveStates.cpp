5fc6cc82d95606cf3eaf1f51ebcd3822744fddb1
jbreslin33 <jbreslin33@localhost>
2011-05-12 20:43:13 -0400

    working. not using stop state as i don't think brian has a need for it. though i left code in there.

--- player/states/moveStates.cpp
+++ player/states/moveStates.cpp
@@ -50,6 +50,157 @@ void Normal_Move::execute(ClientSidePlayer* player)
 	   //if server moving and client needs to catchup
 	   if(player->mClient->mCommand.mCatchup == true && player->mClient->mCommand.mStop == false)
 	   {
+		   //this is catcup state...
+		   player->moveStateMachine->changeState(Catchup_Move::Instance());
+	   }
+	   else //server stopped or we are in sync so just use server vel as is
+	   {
+		  //this is normal state...
+
+
+          Ogre::Vector3 serverDest  = Ogre::Vector3::ZERO;
+		  Ogre::Vector3 myDest      = Ogre::Vector3::ZERO;
+
+	      serverDest.x = player->mClient->mServerFrame.mVelocity.x;
+		  serverDest.z = player->mClient->mServerFrame.mVelocity.z;
+		  serverDest.normalise();
+          serverDest = serverDest * player->runSpeed/1000.0;
+
+		  player->mClient->mCommand.mVelocity.x = serverDest.x;
+	      player->mClient->mCommand.mVelocity.z = serverDest.z;
+
+	   }
+}
+void Normal_Move::exit(ClientSidePlayer* player)
+{
+}
+
+Catchup_Move* Catchup_Move::Instance()
+{
+  static Catchup_Move instance;
+  return &instance;
+}
+void Catchup_Move::enter(ClientSidePlayer* player)
+{
+	LogString("STATE: Catchup_Move");
+}
+void Catchup_Move::execute(ClientSidePlayer* player)
+{
+
+	float deltaX = player->mClient->mServerFrame.mOrigin.x - player->mShape->getSceneNode()->getPosition().x;
+    float deltaZ = player->mClient->mServerFrame.mOrigin.z - player->mShape->getSceneNode()->getPosition().z;
+
+	//distance we are off from server
+	float dist = sqrt(pow(deltaX, 2) + pow(deltaZ, 2));
+
+	  // if distance exceeds threshold
+	  if(dist > player->posInterpLimitHigh)
+	   {
+          player->mClient->mCommand.mCatchup = true;
+	   }
+
+	   //if we are back in sync
+	   if(dist < player->posInterpLimitLow)
+          player->mClient->mCommand.mCatchup = false;
+
+	   // if server has come to a stop
+	   if(player->mClient->mServerFrame.mVelocity.x == 0.0 && player->mClient->mServerFrame.mVelocity.z == 0.0)
+	   {
+		   player->mClient->mCommand.mStop = true;
+	   }
+	   else //server still moving
+	   {
+          player->mClient->mCommand.mStop = false;
+	   }
+    
+	   //if server moving and client needs to catchup
+	   if(player->mClient->mCommand.mCatchup == true && player->mClient->mCommand.mStop == false)
+	   {
+		    Ogre::Vector3 serverDest  = Ogre::Vector3::ZERO; //vector to future server pos
+		    Ogre::Vector3 myDest      = Ogre::Vector3::ZERO; //vector from clienr pos to future server pos
+
+			serverDest.x = player->mClient->mServerFrame.mVelocity.x;
+			serverDest.z = player->mClient->mServerFrame.mVelocity.z;
+			serverDest.normalise();
+
+			float multiplier = dist * player->posInterpFactor;
+			serverDest = serverDest * multiplier;
+			serverDest.x = player->mClient->mServerFrame.mOrigin.x + serverDest.x;
+			serverDest.z = player->mClient->mServerFrame.mOrigin.z + serverDest.z;
+
+			myDest.x = serverDest.x - player->mShape->getSceneNode()->getPosition().x;
+			myDest.z = serverDest.z - player->mShape->getSceneNode()->getPosition().z;
+
+            //dist from clienr pos to future server pos
+			float predictDist = pow(myDest.x, 2) + pow(myDest.z, 2);
+			predictDist = sqrt(predictDist);
+
+			//server velocity
+			float vel = sqrt(pow(player->mClient->mServerFrame.mVelocity.x, 2) + pow(player->mClient->mServerFrame.mVelocity.z, 2))/player->mClient->mCommand.mMilliseconds;
+			//time needed to get to future server pos
+			float time = dist * player->posInterpFactor/(player->runSpeed/1000.0);
+
+			myDest.normalise();
+
+			//client vel needed to get to future server pos in time
+			myDest = myDest * predictDist/time;
+
+			player->mClient->mCommand.mVelocity.x = myDest.x;
+	        player->mClient->mCommand.mVelocity.z = myDest.z;
+	   }
+	   else //server stopped or we are in sync so just use server vel as is
+	   {
+
+		   //this is normal state...
+		   player->moveStateMachine->changeState(Normal_Move::Instance());
+	   }
+}
+void Catchup_Move::exit(ClientSidePlayer* player)
+{
+}
+
+
+Stop_Move* Stop_Move::Instance()
+{
+  static Stop_Move instance;
+  return &instance;
+}
+void Stop_Move::enter(ClientSidePlayer* player)
+{
+	LogString("STATE: Stop_Move");
+}
+void Stop_Move::execute(ClientSidePlayer* player)
+{
+
+	float deltaX = player->mClient->mServerFrame.mOrigin.x - player->mShape->getSceneNode()->getPosition().x;
+    float deltaZ = player->mClient->mServerFrame.mOrigin.z - player->mShape->getSceneNode()->getPosition().z;
+
+	//distance we are off from server
+	float dist = sqrt(pow(deltaX, 2) + pow(deltaZ, 2));
+
+	  // if distance exceeds threshold
+	  if(dist > player->posInterpLimitHigh)
+	   {
+          player->mClient->mCommand.mCatchup = true;
+	   }
+
+	   //if we are back in sync
+	   if(dist < player->posInterpLimitLow)
+          player->mClient->mCommand.mCatchup = false;
+
+	   // if server has come to a stop
+	   if(player->mClient->mServerFrame.mVelocity.x == 0.0 && player->mClient->mServerFrame.mVelocity.z == 0.0)
+	   {
+		   player->mClient->mCommand.mStop = true;
+	   }
+	   else //server still moving
+	   {
+          player->mClient->mCommand.mStop = false;
+	   }
+    
+	   //if server moving and client needs to catchup
+	   if(player->mClient->mCommand.mCatchup == true && player->mClient->mCommand.mStop == false)
+	   {
 		    Ogre::Vector3 serverDest  = Ogre::Vector3::ZERO; //vector to future server pos
 		    Ogre::Vector3 myDest      = Ogre::Vector3::ZERO; //vector from clienr pos to future server pos
 
@@ -96,41 +247,6 @@ void Normal_Move::execute(ClientSidePlayer* player)
 	      player->mClient->mCommand.mVelocity.z = serverDest.z;
 
 	   }
-}
-void Normal_Move::exit(ClientSidePlayer* player)
-{
-}
-
-Catchup_Move* Catchup_Move::Instance()
-{
-  static Catchup_Move instance;
-  return &instance;
-}
-void Catchup_Move::enter(ClientSidePlayer* player)
-{
-	LogString("STATE: Catchup_Move");
-}
-void Catchup_Move::execute(ClientSidePlayer* player)
-{
-
-}
-void Catchup_Move::exit(ClientSidePlayer* player)
-{
-}
-
-
-Stop_Move* Stop_Move::Instance()
-{
-  static Stop_Move instance;
-  return &instance;
-}
-void Stop_Move::enter(ClientSidePlayer* player)
-{
-	LogString("STATE: Stop_Move");
-}
-void Stop_Move::execute(ClientSidePlayer* player)
-{
-
 }
 void Stop_Move::exit(ClientSidePlayer* player)
 {
