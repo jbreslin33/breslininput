e3d7abc57a008b2cf6f52129f20ce2242ba0cfd1
jbreslin33 <jbreslin33@localhost>
2011-05-15 20:47:14 -0400

    latest needs work.

--- tdreamsock/dreamSockLog.cpp
+++ tdreamsock/dreamSockLog.cpp
@@ -23,20 +23,18 @@ dreamConsole *console = NULL;
 void StartLog(void)
 {
 	time_t current = time(NULL);
-	
-	if((fopen_s(&LogFile,"dreamSock.log", "w")))
+
+	if((LogFile = fopen("dreamSock.log", "w")) != NULL)
 	{
 		fprintf(LogFile, "Log file started %s", ctime(&current));
 		
-	//	fclose(LogFile);
+		fclose(LogFile);
 	}
 
-	//fopen_s(&LogFile,"dreamSock.log", "w");
-	
-	
-
+	if((LogFile = fopen("dreamSock.log", "a")) != NULL)
+	{
+	}
 }
-
 
 #ifdef WIN32
 void StartLogConsole(void)
@@ -64,15 +62,14 @@ void LogString(const char *string, ...)
 	char buf[1024];
 	va_list ap;
 	va_start(ap, string);
-	size_t t = 256;
-	vsprintf_s(buf, t,string, ap);
+	vsprintf(buf, string, ap);
 	va_end(ap);
 
 	// Get current time and date
 	time_t current = time(NULL);
 
 	char timedate[64];
-	sprintf_s(timedate,t, ctime(&current));
+	sprintf(timedate, ctime(&current));
 
 	// Remove linefeed from time / date string
 	int i = 0;
@@ -86,7 +83,7 @@ void LogString(const char *string, ...)
 
 	// Output log string
 #ifdef WIN32
-	fprintf_s(LogFile, "%s: %s\n", timedate, buf);
+	fprintf(LogFile, "%s: %s\n", timedate, buf);
 
 	if(console)
 		console->println(buf, 0);
@@ -121,11 +118,10 @@ void dreamConsole::println(char *string, int type, ...)
 	char buf2[1024];
 	va_list ap;
 	va_start(ap, string);
-	size_t t = 256;
-	vsprintf_s(buf, t,string, ap);
+	vsprintf(buf, string, ap);
 	va_end(ap);
 
-	sprintf_s(buf2, t,"-> %s\n", buf);
+	sprintf(buf2, "-> %s\n", buf);
 
 	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 
