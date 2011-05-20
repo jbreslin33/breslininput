da79e96a6cbf663638f14f0c3ca3dd98308c3103
jbreslin33 <jbreslin33@localhost>
2011-05-20 11:01:24 -0400

    pulled out stuff from dreamSock

--- tdreamsock/dreamSock.cpp
+++ tdreamsock/dreamSock.cpp
@@ -1,44 +1,7 @@
-//
-// DreamSock network-library
-//
-// Programmers:
-// Teijo Hakala
-//
-/////////////////////////////////////////////////////////
-
-//#define WIN32
+#include "dreamSock.h"
 
 #include "../message/message.h"
-
-#ifdef WIN32
-// Windows specific headers
-	#ifndef _WINSOCKAPI_
-	#define _WINSOCKAPI_
-	#include <windows.h>
-	#include <winsock2.h>
-
-	#endif
-#else
-// UNIX specific headers
-	#include <memory.h>
-	#include <errno.h>
-	#include <sys/ioctl.h>
-	#include <sys/socket.h>
-	#include <sys/time.h>
-	#include <netinet/in.h>
-	#include <arpa/inet.h>
-#endif
-
-// Common headers
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <time.h>
-#include "dreamSock.h"
 #include "dreamSockLog.h"
-
-
 
 bool dreamSock_init = false;
 
