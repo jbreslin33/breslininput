b3eaabb134e45f9a40e5ded089ab0bd9bd008636
jbreslin33 <jbreslin33@localhost>
2011-05-23 21:19:22 -0400

    cleand more headers.

--- server/server.cpp
+++ server/server.cpp
@@ -8,34 +8,6 @@
 #include "../player/serverSidePlayer.h"
 #include "../game/serverSideGame.h"
 
-#ifdef WIN32
-// Windows specific headers
-	#ifndef _WINSOCKAPI_
-	#define _WINSOCKAPI_
-	#include <windows.h>
-	#include <winsock2.h>
-
-	#endif
-#else
-// UNIX specific headers
-	#include <memory.h>
-	#include <errno.h>
-	#include <sys/ioctl.h>
-	#include <sys/socket.h>
-	#include <sys/time.h>
-	#include <netinet/in.h>
-	#include <arpa/inet.h>
-#endif
-
-// Common headers
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <time.h>
-
-
-
 Server::Server(ServerSideGame* serverSideGame,const char *localIP, int serverPort)
 {
 	init			= false;
