4781558d1ec0ee2bcf8391cb33e0fc338ebe465b
jbreslin33 <jbreslin33@localhost>
2011-05-30 22:56:51 -0400

    comments on what iv'e done. plus ready for brian.

--- shape/serverSideShape.cpp
+++ shape/serverSideShape.cpp
@@ -21,20 +21,26 @@ ServerSideShape::~ServerSideShape()
 
 void ServerSideShape::processTick()
 {
-	
+	setKeyDirection();
 
-setKeyDirection();
-
+	//don't do any rotation if there is no mKey set
+	//should we though? we do it for movement below
+	//does rotation not get to do the same?
+	//is this why the rotation gets off???
     if (mCommand.mKey != 0)
 	{
 		ServerSideRotation::processTick();
 	}
+
+	//even though there is no mKey set we still need to move as
+	//brian has put in a deceleration factor so let's let it run...
 	ServerSideMove::processTick();
 
-
-
-
 	//set all vars to be sent off to clients playing on internets
+	//none of this actually moves anything on server it is what is
+	//going to be sent to clients so it's not in move or rotation.
+	//though it could be i guess, velocity and position could go in move
+	//and mRot could go in rotation.
     mCommand.mVelocity.x = mSceneNode->getPosition().x - mCommand.mOrigin.x;
     mCommand.mVelocity.z = mSceneNode->getPosition().z - mCommand.mOrigin.z;
     mCommand.mVelocity.y = mSceneNode->getPosition().y - mCommand.mOrigin.y;
