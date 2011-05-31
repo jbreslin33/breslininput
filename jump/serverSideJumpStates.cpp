f143db8e8705c13de1b107eca9a96b8a7abf2eb5
jbreslin33 <jbreslin33@localhost>
2011-05-31 00:08:22 -0400

    closer now the server uses states but just one state so it's kinda meaningless, but this is now setup to break into different states, suchas JUMPING, NOT_JUMPING.... ROTATING, NOT_ROTATING.....MOVING, NOT_MOVING, DECELARTING, ACCELERATING. this is all on the server of course the client does different things though still uses classes and states in much the same way.

--- jump/serverSideJumpStates.cpp
+++ jump/serverSideJumpStates.cpp
@@ -3,6 +3,9 @@
 
 #include "serverSideJump.h"
 
+//key defines prob should be changed to a variable if possible
+#define KEY_SPACE				16
+
 Normal_Jump* Normal_Jump::Instance()
 {
   static Normal_Jump instance;
@@ -14,7 +17,29 @@ void Normal_Jump::enter(ServerSideJump* jump)
 }
 void Normal_Jump::execute(ServerSideJump* jump)
 {
-	
+	float clientFrametime;
+    clientFrametime = jump->mCommand.mMilliseconds / 1000.0f;;
+
+    if(jump->mCommand.mKey & KEY_SPACE) 
+    {
+		if(!jump->mJumping)
+		{
+			jump->startJump();
+	    }
+    }
+
+    if(jump->mJumping)
+    {
+        jump->mSceneNode->translate(0, clientFrametime * jump->mVerticalVelocity, 0, Node::TS_LOCAL);
+        jump->mVerticalVelocity -= jump->mGravity * clientFrametime;
+
+        if(jump->mSceneNode->getPosition().y < 0.0)
+        {
+            jump->mSceneNode->setPosition(jump->mSceneNode->getPosition().x, 0.0, jump->mSceneNode->getPosition().z);
+            jump->mVerticalVelocity = 0.0;
+            jump->mJumping = false;
+        }
+	}
 }
 void Normal_Jump::exit(ServerSideJump* jump)
 {
--- rotation/serverSideRotationStates.cpp
+++ rotation/serverSideRotationStates.cpp
@@ -3,6 +3,8 @@
 
 #include "serverSideRotation.h"
 
+#define TURN_SPEED 250.0f      // character turning in degrees per second
+
 Normal_Rotation* Normal_Rotation::Instance()
 {
   static Normal_Rotation instance;
@@ -13,7 +15,45 @@ void Normal_Rotation::enter(ServerSideRotation* rotation)
 }
 void Normal_Rotation::execute(ServerSideRotation* rotation)
 {
-	
+	float clientFrametime;
+
+    clientFrametime = rotation->mCommand.mMilliseconds / 1000.0f;;
+
+	rotation->mGoalDirection = Vector3::ZERO;   // we will calculate this
+
+	Real yawAtSpeed;
+
+	rotation->mGoalDirection += rotation->mKeyDirection.z * Vector3::UNIT_Z;
+    rotation->mGoalDirection += rotation->mKeyDirection.x * Vector3::UNIT_X;
+    rotation->mGoalDirection.y = 0;
+    rotation->mGoalDirection.normalise();
+
+    Quaternion toGoal = rotation->mSceneNode->getOrientation().zAxis().getRotationTo
+		(rotation->mGoalDirection,Vector3::UNIT_Y);
+    
+	// calculate how much the character has to turn to face goal direction
+    Real yawToGoal = toGoal.getYaw().valueDegrees();
+
+    // this is how much the character CAN turn this frame
+    if(yawToGoal == 0.0)
+	{
+		yawAtSpeed = 0.0;
+	}
+    else
+	{
+		yawAtSpeed = yawToGoal / Math::Abs(yawToGoal) * clientFrametime * TURN_SPEED;
+	}
+
+    // turn as much as we can, but not more than we need to
+    if (yawToGoal < 0)
+	{
+		yawToGoal = std::min<Real>(0, std::max<Real>(yawToGoal, yawAtSpeed)); //yawToGoal = Math::Clamp<Real>(yawToGoal, yawAtSpeed, 0);
+	}         
+	else if (yawToGoal > 0)
+	{
+		yawToGoal = std::max<Real>(0, std::min<Real>(yawToGoal, yawAtSpeed)); //yawToGoal = Math::Clamp<Real>(yawToGoal, 0, yawAtSpeed);
+	}               
+    rotation->mSceneNode->yaw(Degree(yawToGoal));
 }
 void Normal_Rotation::exit(ServerSideRotation* rotation)
 {
