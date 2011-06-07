7b4930eb2ca5675cababaeb3dd70a137adb4e5ce
jbreslin33 <jbreslin33@localhost>
2011-06-07 11:56:36 -0400

    this shows aiplayer moving. if you don't want it to move just comment out inside fakeDeltaReadMoveCommand... I am going to add ai functionalilty that is better than this just going to figure out how.

--- serverside/game/game.h
+++ serverside/game/game.h
@@ -60,6 +60,7 @@ public:
 	void	SendCommand(void);
 	void	SendExitNotification(void);
 	void	ReadDeltaMoveCommand(Message *mes, Client *client);
+	void    fakeReadDeltaMoveCommand(Shape* shape);
 	void	BuildMoveCommand(Message *mes, Shape* shape);
 	void	BuildDeltaMoveCommand(Message *mes, Shape* shape);
 	void    runAI();
