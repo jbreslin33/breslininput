b0ad747a110b4934e0e6cf2804354132709803ad
jbreslin33 <jbreslin33@localhost>
2011-06-13 00:04:34 -0400

    dfdfdf

--- serverside/ai/ai.cpp
+++ serverside/ai/ai.cpp
@@ -34,8 +34,8 @@ AI::AI(Vector3D* position, Ogre::Root* root)
 
  	//ai states
 	mAIStateMachine = new AIStateMachine(this);    //setup the state machine
-	mAIStateMachine->setCurrentState      (Normal_AI::Instance());
-	mAIStateMachine->setPreviousState     (Normal_AI::Instance());
+	mAIStateMachine->setCurrentState      (Random_AI::Instance());
+	mAIStateMachine->setPreviousState     (Random_AI::Instance());
 	mAIStateMachine->setGlobalState       (NULL);
 }
 
