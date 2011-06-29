943215206fc0a19002c3610c4dd04ae3970e408d
jbreslin33 <jbreslin33@localhost>
2011-06-29 11:37:08 -0400

    just write directly don't use message AddSequence.

--- clientside/game/game.cpp
+++ clientside/game/game.cpp
@@ -268,7 +268,9 @@ void Game::SendCommand(void)
 
 	message.Init(data, sizeof(data));
 	message.WriteByte(USER_MES_FRAME);						// type
-	message.AddSequences(mClient);					// sequences
+//	message.AddSequences(mClient);					// sequences
+	message.WriteShort(mClient->mOutgoingSequence);
+	message.WriteShort(mClient->mIncomingSequence);
 
 	// Build delta-compressed move command
 	BuildDeltaMoveCommand(&message);
