b0df0dedd99a0cae40303678bcfe4ee67538e463
jbreslin33 <jbreslin33@localhost>
2011-07-26 09:47:06 -0400

    fdf

--- clientside/client/client.h
+++ clientside/client/client.h
@@ -18,10 +18,12 @@ Client(const char *localIP, const char *remoteIP, int port);
 ~Client();
 
 	Message	mMessage;
-	
-	static const char mMessageConnect = -101;
-	static const char mMessageDisconnect = -102;
-	static const char mMessageAddShape = -103;
+
+	//used to direct messages to correct functions, the message header,
+	//not to confused with packet header which deals with with networking.
+	static const char mMessageConnect     = -101;
+	static const char mMessageDisconnect  = -102;
+	static const char mMessageAddShape    = -103;
 	static const char mMessageRemoveShape = -104;
 
 	static const char mMessageConnecting    = 0;
@@ -29,29 +31,31 @@ Client(const char *localIP, const char *remoteIP, int port);
 	static const char mMessageDisconnecting = 2;
 	static const char mMessageDisconnected  = 4;
 
+	//the avatar
     DynamicShape* mShape;  //on server: everybody's got one ...same on clientside mShape is the clients personal avatar..
+	
+	//handles telegraphing key presses etc to server
 	Command mClientCommandToServerArray[64];
-	Command	mClientCommandToServer; //for the human moves to be sent off to server		
+	Command	mClientCommandToServer; //for the human moves to be sent off to server
+
+	//allows communication to server, sending and recieving packtes
 	Network* mNetwork;
 	int				mServerPort;				// Port
-	int				mLastMessageTime;
-
-	void			DumpBuffer(void);
-	void			ParsePacket(Message *mes);
-	void            Reset();
-	int				mConnectionState;		// Connecting, connected, disconnecting, disconnected
-
 	unsigned short	mOutgoingSequence;		// OutFgoing packet sequence
 	unsigned short	mIncomingSequence;		// Incoming packet sequence
 	unsigned short	mIncomingAcknowledged;	// Last packet acknowledged by other end
 	unsigned short	mDroppedPackets;			// Dropped packets
+	int				mConnectionState;		// Connecting, connected, disconnecting, disconnected
 
+	//packets
+	int				GetPacket(char *data);
+	void			SendPacket(Message *message);
+	void			DumpBuffer(void);
+	void			ParsePacket(Message *mes);
+	void            Reset();
+
+	//connection. why it needs it's own method i don't know i'll look into it
 	void			SendConnect(const char *name);
 	void			SendDisconnect(void);
-
-	int				GetPacket(char *data);
-
-	void			SendPacket(Message *message);
-
 };
 #endif
