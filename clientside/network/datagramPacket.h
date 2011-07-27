f087466c99b1658bd6aeea7ea0f4439c7852da74
jbreslin33 <jbreslin33@localhost>
2011-07-27 18:28:33 -0400

    getting closer to java functionality

--- clientside/network/datagramPacket.h
+++ clientside/network/datagramPacket.h
@@ -4,9 +4,20 @@
 class DatagramPacket 
 {
 public:
+
+char* mDataBuffer;	
+	
 DatagramPacket(char buffer[32], int length, const char serverIP[32], int serverPort);
 DatagramPacket(char buffer[32], int length);
 ~DatagramPacket();
+
+const char*  mAddress;
+int          mPort;
+
+int   mLength;
+
+char* getData();
+int   getLength();
 
 };
 #endif
--- clientside/network/datagramSocket.h
+++ clientside/network/datagramSocket.h
@@ -1,11 +1,15 @@
 #ifndef DATAGRAMSOCKET_H
 #define DATAGRAMSOCKET_H
 
+class DatagramPacket;
+
 class DatagramSocket 
 {
 public:
 DatagramSocket(int serverPort);
 ~DatagramSocket();
 
+void send(DatagramPacket* packet);
+void receive(DatagramPacket* packet);
 };
 #endif
