bddc674929e5835dac52eb78da5b5b21cd269fc4
jbreslin33 <jbreslin33@localhost>
2011-07-30 15:36:01 -0400

    fdf

--- clientside/network/datagramPacket.cpp
+++ clientside/network/datagramPacket.cpp
@@ -1,23 +1,15 @@
 #include "datagramPacket.h"
-/*
-DatagramPacket::DatagramPacket(char* dataBuffer, int length, const char address[32], int port)
+
+DatagramPacket::DatagramPacket(char buf[], int length, const char address[32], int port)
 {
-	mDataBuffer = dataBuffer;
+	mDataBuffer = buf;
 	mLength     = length;
 	mAddress    = address;
 	mPort       = port;
 }
-*/
-DatagramPacket::DatagramPacket(char dataBuffer[], int length, const char address[32], int port)
+DatagramPacket::DatagramPacket(char buf[], int length)
 {
-	mDataBuffer = dataBuffer;
-	mLength     = length;
-	mAddress    = address;
-	mPort       = port;
-}
-DatagramPacket::DatagramPacket(char* dataBuffer, int length)
-{
-	mDataBuffer = dataBuffer;
+	mDataBuffer = buf;
 	mLength     = length;
 }
 DatagramPacket::~DatagramPacket()
