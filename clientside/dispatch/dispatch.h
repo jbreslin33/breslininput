c4e153b08c1b75b416ed275f8e0f99be20e2b38a
jbreslin33 <jbreslin33@localhost>
2011-07-30 14:47:18 -0400

    using dispatch class for atleast join command.

--- clientside/dispatch/dispatch.h
+++ clientside/dispatch/dispatch.h
@@ -9,26 +9,32 @@ public:
 	Dispatch();
 	~Dispatch();
 
-private:
 	bool			overFlow;
 	int				maxSize;
 	int				size;
 	int				readCount;
+	char*			data;
+	char			outgoingData[1400];
 
+	//admin
+	int             getReadCount() { return readCount; }
+	void			BeginReading(void);
+	void			BeginReading(int s);
 	char			*GetNewPoint(int length);
-
-public:
-	//void			Init(char *d, int length);
 	void			Clear(void);
-	void			Write(const void *d, int length);
+	bool			GetOverFlow(void)	{ return overFlow; }
+	int				GetSize(void)		{ return size; }
+	void			SetSize(int s)		{ size = s; }
 
+	//write
+	void			Write(const void *d, int length);
 	void			WriteByte(char c);
 	void			WriteShort(short c);
 	void			WriteLong(long c);
 	void			WriteFloat(float c);
 	void			WriteString(const char *s);
-	void			BeginReading(void);
-	void			BeginReading(int s);
+
+	//read
 	char			*Read(int s);
 	char			ReadByte(void);
 	short			ReadShort(void);
@@ -36,14 +42,6 @@ public:
 	float			ReadFloat(void);
 	char			*ReadString(void);
 
-	bool			GetOverFlow(void)	{ return overFlow; }
-	int				GetSize(void)		{ return size; }
-	void			SetSize(int s)		{ size = s; }
-
-	char*			data;
-	char			outgoingData[1400];
-
-	int             getReadCount() { return readCount; }
 };
 
 
