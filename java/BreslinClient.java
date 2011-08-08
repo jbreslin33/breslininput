c83edc1202a5c7ae241fad98975ad34cb792af8b
jbreslin33 <jbreslin33@localhost>
2011-08-08 11:53:29 -0400

    fdf

--- java/BreslinClient.java
+++ java/BreslinClient.java
@@ -6,26 +6,16 @@ public class BreslinClient {
   public static void main(String[] argv) throws Exception {
 
 	DatagramChannel datagramChannel = DatagramChannel.open();
-	//datagramChannel.configureBlocking(false);
-	//datagramChannel.socket().bind(new InetSocketAddress("192.168.1.103",9999));
 
-	//ByteBuffer byteBuffer = ByteBuffer.allocate(48);
-	//byteBuffer.clear();
-
-	//datagramChannel.receive(byteBuffer);
-
-
-
-
-String newData = "New String to write to file..."
+	String newData = "New String to write to file..."
                     + System.currentTimeMillis();
 
-ByteBuffer buf = ByteBuffer.allocate(48);
-buf.clear();
-buf.put(newData.getBytes());
-buf.flip();
+	ByteBuffer buf = ByteBuffer.allocate(48);
+	buf.clear();
+	buf.put(newData.getBytes());
+	buf.flip();
 
-int bytesSent = datagramChannel.send(buf, new InetSocketAddress("192.168.1.103", 9999));
+	int bytesSent = datagramChannel.send(buf, new InetSocketAddress("192.168.1.103", 9999));
 
 
   }
