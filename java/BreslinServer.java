ccadd076127582994ca69b2425f6973c07b509f1
jbreslin33 <jbreslin33@localhost>
2011-08-08 12:04:14 -0400

    breslinServer and BreslinClient work as expected, breslinServer does not block but loops

--- java/BreslinServer.java
+++ java/BreslinServer.java
@@ -2,20 +2,32 @@ import java.net.InetSocketAddress;
 import java.nio.channels.DatagramChannel;
 import java.nio.ByteBuffer;
 
-public class BreslinServer {
-  public static void main(String[] argv) throws Exception {
+public class BreslinServer
+{
+	public static void main(String[] argv) throws Exception
+	{
+
+		DatagramChannel datagramChannel = DatagramChannel.open();
+		datagramChannel.configureBlocking(false);
+		datagramChannel.socket().bind(new InetSocketAddress(9999));
+
+		while (true)
+		{
 
 
 
-	DatagramChannel datagramChannel = DatagramChannel.open();
-	datagramChannel.configureBlocking(true);
-	datagramChannel.socket().bind(new InetSocketAddress(9999));
+			ByteBuffer byteBuffer = ByteBuffer.allocate(48);
+			byteBuffer.clear();
 
-	ByteBuffer byteBuffer = ByteBuffer.allocate(48);
-	byteBuffer.clear();
+			if(datagramChannel.receive(byteBuffer) != null)
+			{
+				System.out.println("got this>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>:");
+			}
+			else
+			{
+				System.out.println("got nothing");
+			}
 
-	datagramChannel.receive(byteBuffer);
-
-
-  }
+		}
+	}
 }
