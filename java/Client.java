edb2c1ea0c021e6c0355f8ced3c14046874d6d76
jbreslin33 <jbreslin33@localhost>
2011-08-08 10:50:31 -0400

    gfg

--- java/Client.java
+++ java/Client.java
@@ -1,119 +1,154 @@
-/**
- * All
 
-Here's a non-blocking IO skeleton, which follows the ideas I presented in class last night.  Be aware that it requires at least java 1.4.
-Note that it creates only 1 DatagramChannel and receives and sends from it.  The DatagramChannel has a built-in socket, but we never use the socket directly.
-
-Just for fun, you can insert the code
-      try
-      {
-         Thread.sleep( (long) (datagramInterval*Math.random ()) );
-      } catch (InterruptedException e)
-      {}
-just before
-               millisecsUntilSendNextDatagram -= System.currentTimeMillis() - start;
-and see that a message is still sent every datagramInterval.
-
-Java™ NIO,By Ron Hitchens is a book on NON-BLOCKING IO by O'Reilly.  if you're willing to give your credit card # you can read it on-line for free for 2 weeks.
-
-One handy thing about ByteBuffers is that they have putInt( index ) and getInt( index ) methods that could be used to handle sequence numbers.
-
-YOU MAY USE THIS CODE IN YOUR RUC PROGRAM.  BE AWARE THAT THE EXCEPTION HANDLING IS COMPLETELY INADEQUATE.
-
-/**
- * @(#)Nonblocking_IO_example.java 1.0 03/03/25
- *
- * @author Arthur Goldberg
- */
-
-
-import java.io.*;
-import java.net.*;
 import java.nio.*;
-import java.nio.channels.*;  // bug? redundant with previous one??
-import java.util.*;
+import java.nio.channels.*;
+import java.net.*;
+import java.io.*;
+import java.nio.channels.spi.*;
+import java.nio.charset.*;
+import java.lang.*;
 
-/**
-  The key ideas, for a 'Non-blocking' implementation of RUC:
-  RUC does 2 network activities:
-  1. Send datagrams, but no faster than Maximum data rate (MAX_RATE)
-  2. Read datagrams, and process them
 
-  To do this in 1 thread, use select() which blocks until one of many
-things happen, such as
-  ·  time elapses
-  ·  a DatagramChannel has data to read
-  ·  a DatagramChannel can be written without waiting
-  ·  etc.
+public class Client
+{
+    public SocketChannel client = null;
+    public InetSocketAddress isa = null;
+    public RecvThread rt = null;
 
-  Use this technique.  Only block in 'select'; let the select return
-whenever either
-  ·  a datagram can be read and processed immediately,
-  ·  its time to send a datagram.
-*/
+    public Client()
+    {
+    }
+    
+	public void makeConnection()
+    {
+		int result = 0;
+		try
+		{
+			
+			client = SocketChannel.open();
+				isa = new InetSocketAddress("nicholson",4900);
+			client.connect(isa);
+			client.configureBlocking(false);
+			receiveMessage();    
+		}
+		catch(UnknownHostException e)
+		{
+			e.printStackTrace();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		while ((result = sendMessage()) != -1)
+		{
+		}
 
-class Client {
+		try
+		{
+			client.close();
+			System.exit(0);
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+    }
+    
+	public int sendMessage()
+    {
+		System.out.println("Inside SendMessage");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String msg = null;
+		ByteBuffer bytebuf = ByteBuffer.allocate(1024);
+		int nBytes = 0;
+		try
+		{
+			msg = in.readLine();
+			System.out.println("msg is "+msg);
+			bytebuf = ByteBuffer.wrap(msg.getBytes());
+			nBytes = client.write(bytebuf);
+			System.out.println("nBytes is "+nBytes);
+			if (msg.equals("quit") || msg.equals("shutdown")) {
+				System.out.println("time to stop the client");
+				interruptThread();
+				try
+				{
+					Thread.sleep(5000);
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+				client.close();
+				return -1;
+			}
+	    
+		}
+        catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		System.out.println("Wrote "+nBytes +" bytes to the server");
+		return nBytes;
+    }
 
-   // set input parameters
-   String host = "localhost";   // "slinky.cs.nyu.edu";
-   int port = 10000;
-   int datagramSize = 512;
-   int datagramInterval = 1000;
-   int numMsgs = 24;
+    public void receiveMessage()
+    {
+		rt = new RecvThread("Receive THread",client);
+		rt.start();
 
-   // array of bytes for  a datagram to send
-   byte[] sendData = new byte[ datagramSize ];
-   ByteBuffer theSendDataByteBuffer = ByteBuffer.wrap( sendData );
+    }
 
-   // array of bytes for receiving datagrams
-   byte[] receiveData = new byte[ datagramSize ];
-   ByteBuffer theReceiveDataByteBuffer = ByteBuffer.wrap( receiveData );
+    public void interruptThread()
+    {
+		rt.val = false;
+    }
 
-   Client()
-   {
-      try
-      {
-         Random theRandom = new Random();
-         InetSocketAddress theInetSocketAddress = new InetSocketAddress( host, port);
+    public static void main(String args[])
+    {
+		Client cl = new Client();
+		cl.makeConnection();
+    }
 
-         // make a DatagramChannel
-         DatagramChannel theDatagramChannel = DatagramChannel.open();
+    public class RecvThread extends Thread
+    {
+		public SocketChannel sc = null;
+		public boolean val = true;
+	
+		public RecvThread(String str,SocketChannel client)
+		{
+			super(str);
+			sc = client;
+		}
+	
+		public void run() {
 
-         // A channel must first be placed in nonblocking mode
-         // before it can be registered with a selector
-         theDatagramChannel.configureBlocking( false );
+			System.out.println("Inside receivemsg");
+			int nBytes = 0;
+			ByteBuffer buf = ByteBuffer.allocate(2048);
+			try
+			{
+				while (val)
+				{
+					while ( (nBytes = nBytes = client.read(buf)) > 0){
+						buf.flip();
+						Charset charset = Charset.forName("us-ascii");
+						CharsetDecoder decoder = charset.newDecoder();
+						CharBuffer charBuffer = decoder.decode(buf);
+						String result = charBuffer.toString();
+									System.out.println(result);
+						buf.flip();
+						
+					}
+				}
+			
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+			
+			}
+            
 
-         // instantiate a selector
-         Selector theSelector = Selector.open();
-
-         // register the selector on the channel to monitor reading
-         // datagrams on the DatagramChannel
-         theDatagramChannel.register( theSelector, SelectionKey.OP_READ );
-
-         long millisecsUntilSendNextDatagram = 0;
-         int i = 1;  int j = 1;
-
-         // send and read concurrently, but do not block on read:
-         while (true)
-         {
-			// read the datagram from the DatagramChannel,
-			System.out.println("ret should not be always null:"+ theDatagramChannel.receive( theReceiveDataByteBuffer ));
-
-	       	j++;
-
-         }
-         //theSelector.close();
-      }
-      catch (Exception e)
-      {
-         e.printStackTrace();
-         System.out.println("Exception " + e);
-         return;
-      }
-   }
-
-   public static void main( String args[] )
-   {
-      new Client();
-   }
+		}
+    }
 }
