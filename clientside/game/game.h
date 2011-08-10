a7eca70870827cc992a2d63ad7655db363ddf6e7
jbreslin33 <jbreslin33@localhost>
2011-08-10 11:20:00 -0400

    got rid of client. infused into game. now cleaning up game. works.

--- clientside/game/game.h
+++ clientside/game/game.h
@@ -3,11 +3,15 @@
 
 #include "BaseApplication.h"
 
+//command
+#include "../../command/command.h"
+
 class Client;
 class MovableTextOverlay;
 class Dispatch;
 class DynamicShape;
 class Time;
+class Network;
 
 class Game : public BaseApplication
 {
@@ -16,18 +20,53 @@ public:
 	Game(const char* serverIP);
 	~Game();
 
-	// Client
-	Client* mClient;
-
-	//Server
-	const char*  mServerIP;
+	/***************************************
+			    MEMBER VARIABLES
+	***************************************/
 
 	//Shapes
 	std::vector<DynamicShape*> mShapeVector;	 //all shapes in the client world
 	std::vector<DynamicShape*> mShapeGhostVector;	 //all shapes in the client world's ghost 
 
 	std::vector<MovableTextOverlay*> myVect;  //for writing above shapes head
-	
+
+	//commands
+	int mCommandHistorySize;
+	Command mClientCommandToServerArray[64];
+	Command	mClientCommandToServer; //for the human moves to be sent off to server
+
+	// Dispatches
+	static const char mCommandKey          = 1;
+	static const char mCommandMilliseconds = 2;
+
+	static const char mMessageFrame = 1;
+	static const char mMessageConnect     = -101;
+	static const char mMessageDisconnect  = -102;
+	static const char mMessageAddShape    = -103;
+	static const char mMessageRemoveShape = -104;
+
+	static const char mMessageConnecting    = 0;
+	static const char mMessageConnected     = 1;
+	static const char mMessageDisconnecting = 2;
+	static const char mMessageDisconnected  = 4;
+
+	static const char mMessageNonDeltaFrame = 2;
+	static const char mMessageServerExit = 3;
+	static const char mMessageKeepAlive = 12;
+
+	//Network
+	Network*     mNetwork;
+	int			 mServerPort;				// Port
+	const char*  mServerIP;
+
+	unsigned short	mOutgoingSequence;		// OutFgoing packet sequence
+	unsigned short	mIncomingSequence;		// Incoming packet sequence
+
+
+	//commands
+	void	sendCommand             ();
+	void	buildDeltaMoveCommand   (Dispatch* dispatch);
+
 	//time
 	Time* mTime;
 	float mFrameTime;
@@ -51,13 +90,9 @@ public:
 	int mKeyRight;
 	int mKeySpace;
 
-	//messages
-	static const char mMessageFrame = 1;
-	static const char mMessageNonDeltaFrame = 2;
-	static const char mMessageServerExit = 3;
-	static const char mMessageKeepAlive = 12;
-
-	
+	/***************************************
+			          METHODS
+	***************************************/
 	//Admin
 	void	shutdown    ();
 	void	gameLoop();
@@ -74,6 +109,11 @@ public:
 	// Network
 	void	runNetwork  (int msec);
 	void	readPackets             ();
+	int				getPacket  (Dispatch* dispatch);
+	void			sendPacket (Dispatch* dispatch);
+	void            reset();
+	void			sendConnect(const char *name);
+	void			sendDisconnect(void);
 
 	//input
 	void processUnbufferedInput();
